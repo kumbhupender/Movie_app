0f290231bbd35a500fa804ac2eace37c78381469
kumbhupender <kumarbhupendra8171@gmail.com>
2023-08-01 14:18:55 +0530

    Adding store:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+
 import "./index.css";
 import App from "./components/App";
+import movies from "./reducers";
+
+//creating a store
+const store = createStore(movies);
+console.log(store);
+console.log("State", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
